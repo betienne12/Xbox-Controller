6ead63eadd0f71ebc363ee86844cf242bff245ff
Brian Etienne <betienne@umassd.edu>
2017-04-06 22:06:23 -0400

    Xbox

---
+++ xbox 3/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace xbox_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.blade_on = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(281, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect Controller";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // blade_on
+            // 
+            this.blade_on.Location = new System.Drawing.Point(1, 131);
+            this.blade_on.Name = "blade_on";
+            this.blade_on.Size = new System.Drawing.Size(112, 57);
+            this.blade_on.TabIndex = 1;
+            this.blade_on.Text = "Blade On";
+            this.blade_on.UseVisualStyleBackColor = true;
+            this.blade_on.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1, 194);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 57);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Blade Off";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(40, 257);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(213, 85);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(167, 27);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(115, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(63, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Speed";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(167, 131);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(115, 57);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Increase Speed";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(167, 194);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(115, 57);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Decrease Speed";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 8000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 354);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.blade_on);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "RC Mower";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button blade_on;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ xbox 3/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpDX;
+using SharpDX.XInput;
+
+namespace xbox_3
+{
+    
+    public partial class Form1 : Form
+    {
+        Controller controller;
+        private State stateOld;
+        public bool connected = false;
+        //private int x, y;
+        private double normalizedLX, normalizedLY;
+        private TcpClient client = null;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = "Controller Settings: \r\nPress X to turn on blade \r\nPress Y to turn off Blade \r\nPress A to increase speed \r\nPress B to decrease speed";
+        }   
+        public void Client_Connect()
+        {
+            try
+            {
+                client = new TcpClient();
+                client.Connect("134.88.143.211", 8001);
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Error.....Can't connect to server");
+            }
+        }
+   
+        public void SendPacket(string str)
+        {
+            string head = "&";
+            string tail = "@";
+            str = head + str + tail; ;
+            Stream stm = client.GetStream();
+            ASCIIEncoding asen = new ASCIIEncoding();
+            byte[] packet = asen.GetBytes(str);
+            stm.Write(packet, 0, packet.Length);   //sendd to server
+        }
+     
+        
+        public void Connect() //conect controller
+        {
+            controller = new Controller(UserIndex.One);
+            connected = controller.IsConnected;
+            if (connected)
+                MessageBox.Show("Controller has successfully connected. Click Ok to continue");
+            if (!connected)
+            {
+                MessageBox.Show("ERROR: Controller did not conncect.");
+                
+            }
+        } 
+
+        void Joystick()
+        {
+            State stick = controller.GetState();
+            // Joystick controlls
+            double x = (stick.Gamepad.LeftThumbX);
+            double y = (stick.Gamepad.LeftThumbY);
+
+           double magnitude = Math.Sqrt(x * x + y * y);
+            //determine the direction the controller is pushed
+            normalizedLX =( x / magnitude)*100;
+            normalizedLY = (y / magnitude)*100;
+            string xval = Convert.ToString(normalizedLX);
+            string yval = Convert.ToString(normalizedLY);
+            MessageBox.Show(xval+ "," + yval);
+        }
+        void GetInput()
+        {
+            State stateNew = controller.GetState(); 
+            //buttons controlls
+            if (this.stateOld.Gamepad.Buttons == GamepadButtonFlags.A && stateNew.Gamepad.Buttons == GamepadButtonFlags.A)
+            {
+                
+                SendPacket("hello");
+            }
+            
+            if (this.stateOld.Gamepad.Buttons == GamepadButtonFlags.B && stateNew.Gamepad.Buttons == GamepadButtonFlags.B)
+            {
+                MessageBox.Show("B pressed");
+            }
+            if (this.stateOld.Gamepad.Buttons == GamepadButtonFlags.X && stateNew.Gamepad.Buttons == GamepadButtonFlags.X)
+            {
+                MessageBox.Show("X pressed");
+            }
+            if (this.stateOld.Gamepad.Buttons == GamepadButtonFlags.Y && stateNew.Gamepad.Buttons == GamepadButtonFlags.Y)
+            {
+                MessageBox.Show("Y pressed");
+            }
+            if (this.stateOld.Gamepad.Buttons == GamepadButtonFlags.Start && stateNew.Gamepad.Buttons == GamepadButtonFlags.Start)
+            {
+                MessageBox.Show("Start pressed");
+            }
+            if (this.stateOld.Gamepad.Buttons == GamepadButtonFlags.DPadDown && stateNew.Gamepad.Buttons == GamepadButtonFlags.DPadDown)
+            {
+                MessageBox.Show("Down pressed");
+            }
+            if (this.stateOld.Gamepad.Buttons == GamepadButtonFlags.DPadUp && stateNew.Gamepad.Buttons == GamepadButtonFlags.DPadUp)
+            {
+                MessageBox.Show("Up pressed");
+            }
+            if (this.stateOld.Gamepad.Buttons == GamepadButtonFlags.DPadLeft && stateNew.Gamepad.Buttons == GamepadButtonFlags.DPadLeft)
+            {
+                MessageBox.Show("L pressed");
+            }
+            if (this.stateOld.Gamepad.Buttons == GamepadButtonFlags.DPadRight && stateNew.Gamepad.Buttons == GamepadButtonFlags.DPadRight)
+            {
+                MessageBox.Show("R pressed");
+            }
+                this.stateOld = stateNew;
+            
+        }
+        private void button1_Click(object sender, EventArgs e)  //Connect button
+        {
+            Connect();
+            //Client_Connect();
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+        }
+        private void button2_Click(object sender, EventArgs e) //blade on button
+        {
+            SendPacket("hello");
+        }
+        private void button3_Click(object sender, EventArgs e) //blade off
+        {
+            //SendPacket("hello");
+        }
+        private void button4_Click(object sender, EventArgs e) //increase speed
+        {
+            //SendPacket("hello");
+        }
+        private void button5_Click(object sender, EventArgs e)  //decrease sppeed
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)  //timer for keep alive connection
+        {
+            //clock started in button1()
+            Joystick();
+
+        }
+
+       
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //clock started in button1()
+            GetInput();
+        }   //timer for updating controller state
+    }
+}
