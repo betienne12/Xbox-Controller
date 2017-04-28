83ea18d964e754fb358cd4b2d8875c76d0581aa9
Brian Etienne <betienne@umassd.edu>
2017-04-28 11:57:23 -0400

    test2

--- xbox 3/Form1.cs
+++ xbox 3/Form1.cs
@@ -53,7 +53,7 @@ namespace xbox_3
 
            
             Collection<String> connectedSsids = new Collection<string>();
-            string network = "LAWNMOWER";
+            string network = "UMASSD-A";
             bool Connection = NetworkInterface.GetIsNetworkAvailable();
             try
             {
@@ -154,121 +154,7 @@ namespace xbox_3
             
             //buttons controlls
            
-            if (this.stateOld.Gamepad.Buttons == GamepadButtonFlags.A && stateNew.Gamepad.Buttons == GamepadButtonFlags.A)  //increase speed
-            {
-                // MessageBox.Show("a pressed");
-                char[] arr = str.ToCharArray();
-                string speed;
-                string bit_1 = (arr[1]).ToString();
-                string bit_2 = (arr[2]).ToString();
-                string val = bit_1 + bit_2;    //2 bit hex value to be added
-                                               // MessageBox.Show(val);
-                if (val == "F0")   //F0 = 240 last value without going over 255
-                {
-                    speed = "FF";
-                    MessageBox.Show("Max speed has been reached");
-                }
-                else
-                {
-                    int intFromHex = int.Parse(val, System.Globalization.NumberStyles.HexNumber) + 20;  //increment hex value by 20
-                    speed = intFromHex.ToString("X");
-                }
-                arr[1] = speed[0];
-                arr[2] = speed[1];
-                arr[3] = speed[0];
-                arr[4] = speed[1];
-                str = new string(arr);
-                SendPacket(str);
-                textBox2.Text = str;
-
-
-            }
-
-            if (this.stateOld.Gamepad.Buttons == GamepadButtonFlags.B && stateNew.Gamepad.Buttons == GamepadButtonFlags.B)  //decrease speed
-            {
-                char[] arr = str.ToCharArray();
-                string speed;
-                string bit_1 = (arr[1]).ToString();
-                string bit_2 = (arr[2]).ToString();
-                string val = bit_1 + bit_2;    //2 bit hex value to be added
-                string dec = Convert.ToString(Convert.ToInt32(val, 16), 10);
-                if (Convert.ToInt32(dec) < 36)
-                {
-                    speed = "00";
-                    MessageBox.Show("Lowest speed has been reached");
-                }
-                
-               
-                else
-                {
-                    int intFromHex = int.Parse(val, System.Globalization.NumberStyles.HexNumber) - 20;  //decrement hex value by 20
-                    speed = intFromHex.ToString("X");
-                }
-                arr[1] = speed[0];
-                arr[2] = speed[1];
-                arr[3] = speed[0];
-                arr[4] = speed[1];
-                str = new string(arr);
-                SendPacket(str);
-                textBox2.Text = str;
-            }
-            if (this.stateOld.Gamepad.Buttons == GamepadButtonFlags.X && stateNew.Gamepad.Buttons == GamepadButtonFlags.X)  //turn on blade
-            {
-                //if (temp[0] == '1')
-                //{
-                //    MessageBox.Show("The Blade is already on");
-                //}
-                if (temp[0] != '1')
-                {
-                    temp = temp.Remove(0, 1).Insert(0, "1");  //100
-                    String binary = Convert.ToString(Convert.ToInt32(temp, 2), 10);
-                    str = str.Remove(0, 1).Insert(0, binary);
-                    SendPacket(str);
-                    textBox2.Text = str;
-                    button3.BackColor = SystemColors.Control;
-                    blade_on.BackColor = System.Drawing.Color.LightSkyBlue;
-                }
-
-            }
-            if (this.stateOld.Gamepad.Buttons == GamepadButtonFlags.Y && stateNew.Gamepad.Buttons == GamepadButtonFlags.Y)  //turn off blade
-            {
-                //if (temp[0] == '0')
-                //{
-                //    MessageBox.Show("The Blade is already off");
-                //}
-
-                if (temp[0] != '0')
-                {
-                    temp = temp.Remove(0, 1).Insert(0, "0");  //000
-                    String binary = Convert.ToString(Convert.ToInt32(temp, 2), 10);
-                    str = str.Remove(0, 1).Insert(0, binary);
-                    SendPacket(str);
-                    button3.BackColor = System.Drawing.Color.LightSkyBlue;
-                    blade_on.BackColor =SystemColors.Control;
-                    textBox2.Text = str;
-                }
-
-            }
-            if (this.stateOld.Gamepad.Buttons == GamepadButtonFlags.Start && stateNew.Gamepad.Buttons == GamepadButtonFlags.Start)
-            {
-                char[] decoy = initial.ToCharArray();
-                char[] arr = temp.ToCharArray();
-                char[] packet = str.ToCharArray();
-                arr[0] = '1';
-                arr[1] = '1';   //assuming 0 is backwards
-                arr[2] = '1';
-                temp = new string(arr);
-
-                packet[1] = decoy[1];
-                packet[2] = decoy[2];
-                packet[3] = decoy[3];
-                packet[4] = decoy[4];
-                str = new string(packet);
-                String binary = Convert.ToString(Convert.ToInt32(temp, 2), 10);
-                str = str.Remove(0, 1).Insert(0, binary);
-                SendPacket(str);
-                textBox2.Text = str;
-            }
+            
             if (this.stateOld.Gamepad.Buttons == GamepadButtonFlags.RightShoulder && stateNew.Gamepad.Buttons == GamepadButtonFlags.RightShoulder)   //stop button
             {
                 char[] arr = str.ToCharArray();
@@ -409,12 +295,152 @@ namespace xbox_3
             }*/
             stateOld = stateNew;
          }
+        private void Stop()
+        {
+            char[] arr = str.ToCharArray();
+            char[] array = temp.ToCharArray();
+            array[0] = '0';
+            array[1] = '0';
+            array[2] = '0';
+            arr[1] = '0';
+            arr[2] = '0';
+            arr[3] = '0';
+            arr[4] = '0';
+            string test = new string(arr);
+            temp = new string(array);
+            String binary = Convert.ToString(Convert.ToInt32(temp, 2), 10);
+            str = str.Remove(0, 1).Insert(0, binary);
+            SendPacket(test);
+            textBox2.Text = test;
+
+        }
        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         private void Navigate()
         {
             State stateNew = controller.GetState();
             if (LeftTrigger > 0)
             {
+                if (this.stateOld.Gamepad.Buttons == GamepadButtonFlags.A && stateNew.Gamepad.Buttons == GamepadButtonFlags.A)  //increase speed
+                {
+                    // MessageBox.Show("a pressed");
+                    char[] arr = str.ToCharArray();
+                    string speed;
+                    string bit_1 = (arr[1]).ToString();
+                    string bit_2 = (arr[2]).ToString();
+                    string val = bit_1 + bit_2;    //2 bit hex value to be added
+                                                   // MessageBox.Show(val);
+                    if (val == "F0")   //F0 = 240 last value without going over 255
+                    {
+                        speed = "FF";
+                        MessageBox.Show("Max speed has been reached");
+                    }
+                    else
+                    {
+                        int intFromHex = int.Parse(val, System.Globalization.NumberStyles.HexNumber) + 20;  //increment hex value by 20
+                        speed = intFromHex.ToString("X");
+                    }
+                    arr[1] = speed[0];
+                    arr[2] = speed[1];
+                    arr[3] = speed[0];
+                    arr[4] = speed[1];
+                    str = new string(arr);
+                    SendPacket(str);
+                    textBox2.Text = str;
+
+
+                }
+
+                if (this.stateOld.Gamepad.Buttons == GamepadButtonFlags.B && stateNew.Gamepad.Buttons == GamepadButtonFlags.B)  //decrease speed
+                {
+                    char[] arr = str.ToCharArray();
+                    string speed;
+                    string bit_1 = (arr[1]).ToString();
+                    string bit_2 = (arr[2]).ToString();
+                    string val = bit_1 + bit_2;    //2 bit hex value to be added
+                    string dec = Convert.ToString(Convert.ToInt32(val, 16), 10);
+                    if (Convert.ToInt32(dec) < 36)
+                    {
+                        speed = "00";
+                        MessageBox.Show("Lowest speed has been reached");
+                    }
+
+
+                    else
+                    {
+                        int intFromHex = int.Parse(val, System.Globalization.NumberStyles.HexNumber) - 20;  //decrement hex value by 20
+                        speed = intFromHex.ToString("X");
+                    }
+                    arr[1] = speed[0];
+                    arr[2] = speed[1];
+                    arr[3] = speed[0];
+                    arr[4] = speed[1];
+                    str = new string(arr);
+                    SendPacket(str);
+                    textBox2.Text = str;
+                }
+
+                if (this.stateOld.Gamepad.Buttons == GamepadButtonFlags.Start && stateNew.Gamepad.Buttons == GamepadButtonFlags.Start)
+                {
+                    char[] decoy = initial.ToCharArray();
+                    char[] arr = temp.ToCharArray();
+                    char[] packet = str.ToCharArray();
+                    arr[0] = '1';
+                    arr[1] = '1';   //assuming 0 is backwards
+                    arr[2] = '1';
+                    temp = new string(arr);
+
+                    packet[1] = decoy[1];
+                    packet[2] = decoy[2];
+                    packet[3] = decoy[3];
+                    packet[4] = decoy[4];
+                    str = new string(packet);
+                    String binary = Convert.ToString(Convert.ToInt32(temp, 2), 10);
+                    str = str.Remove(0, 1).Insert(0, binary);
+                    SendPacket(str);
+                    textBox2.Text = str;
+                }
+              
+                if (this.stateOld.Gamepad.Buttons == GamepadButtonFlags.X && stateNew.Gamepad.Buttons == GamepadButtonFlags.X)  //turn on blade
+                {
+                    if (temp[0] != '1')
+                    {
+                       
+                        char[] speed = str.ToCharArray();
+                        speed[1] = '0';
+                        speed[2] = '0';
+                        speed[3] = '0';
+                        speed[4] = '0';
+                        temp = temp.Remove(0, 1).Insert(0, "1");  //100
+                       
+                        str = new string(speed);
+                        String binary = Convert.ToString(Convert.ToInt32(temp, 2), 10);
+                        str = str.Remove(0, 1).Insert(0, binary);
+                        SendPacket(str);
+                        textBox2.Text = str;
+                        button3.BackColor = SystemColors.Control;
+                        blade_on.BackColor = System.Drawing.Color.LightSkyBlue;
+                    }
+
+                }
+                if (this.stateOld.Gamepad.Buttons == GamepadButtonFlags.Y && stateNew.Gamepad.Buttons == GamepadButtonFlags.Y)  //turn off blade
+                {
+                   
+                        char[] speed = str.ToCharArray();
+                        speed[1] = '0';
+                        speed[2] = '0';
+                        speed[3] = '0';
+                        speed[4] = '0';
+                        temp = temp.Remove(0, 1).Insert(0, "0");  //000
+                        str = new string(speed);
+                        String binary = Convert.ToString(Convert.ToInt32(temp, 2), 10);
+                        str = str.Remove(0, 1).Insert(0, binary);
+                        SendPacket(str);
+                        button3.BackColor = System.Drawing.Color.LightSkyBlue;
+                        blade_on.BackColor = SystemColors.Control;
+                        textBox2.Text = str;
+                    
+
+                }
                 if ((this.stateOld.Gamepad.Buttons == GamepadButtonFlags.DPadDown && stateNew.Gamepad.Buttons == GamepadButtonFlags.DPadDown)) // && (LeftTrigger > 0))
                 {
                         //Speed_Reset();
@@ -487,7 +513,7 @@ namespace xbox_3
                     
                 }
             }
-            else
+            if(LeftTrigger==0)
             {
                 char[] arr = str.ToCharArray();
                 char[] array = temp.ToCharArray();
@@ -516,17 +542,26 @@ namespace xbox_3
         }
         private void button2_Click(object sender, EventArgs e) //blade on button
         {
-           
-            if (temp[0] != '1')
+            State j = controller.GetState();
+            LeftTrigger = j.Gamepad.LeftTrigger;
+            if (LeftTrigger == 0 || LeftTrigger > 0)
             {
+                char[] speed = str.ToCharArray();
+                speed[1] = '0';
+                speed[2] = '0';
+                speed[3] = '0';
+                speed[4] = '0';
                 temp = temp.Remove(0, 1).Insert(0, "1");  //100
+
+                str = new string(speed);
                 String binary = Convert.ToString(Convert.ToInt32(temp, 2), 10);
                 str = str.Remove(0, 1).Insert(0, binary);
                 SendPacket(str);
+                textBox2.Text = str;
                 button3.BackColor = SystemColors.Control;
                 blade_on.BackColor = System.Drawing.Color.LightSkyBlue;
-                textBox2.Text = str;
             }
+        
         }
         private void button3_Click(object sender, EventArgs e) //blade off
         {
